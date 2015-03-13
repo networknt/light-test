77a8164ccd018f45488dd1a970dd54cd375234a3
Steve Hu <stevehu@gmail.com>
2015-03-12 23:15:56 -0400

    cors handling in the rule

--- rule-test/src/main/java/com/networknt/light/rule/example/main/perf/HelloWorld_1Rule.java
+++ rule-test/src/main/java/com/networknt/light/rule/example/main/perf/HelloWorld_1Rule.java
@@ -2,20 +2,19 @@ package com.networknt.light.rule.example.main.perf;
 
 import com.networknt.light.rule.AbstractRule;
 import com.networknt.light.rule.Rule;
-import com.networknt.light.util.ServiceLocator;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.Map;
 
 /**
- * Created by steve on 02/02/15.
+ * Created by steve on 16/02/15.
  */
-public class HelloWorldRule extends AbstractRule implements Rule {
+public class HelloWorld_1Rule extends AbstractRule implements Rule {
     private static final org.slf4j.Logger logger = LoggerFactory.getLogger(HelloWorldRule.class);
     public boolean execute (Object ...objects) throws Exception {
         Map<String, Object> inputMap = (Map<String, Object>) objects[0];
         logger.debug(this.toString());
-        inputMap.put("result", "{\"message\": \"Hello World\"}");
+        inputMap.put("result", "{\"message\": \"Hello World 1\"}");
         return true;
     }
 }
